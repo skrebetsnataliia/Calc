e70db0f79da08b00a580e2a7e6d9b04489fc0184
skrebetsnataliia <skrebetsnatasha98@gmail.com>
2020-02-14 10:56:22 +0200

    added comments

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
+//open modal window
 document.querySelector(".btn-open").addEventListener( "click", ()=>{
     document.querySelector(".modal").style.display = "block";
 });
 
+//close modal window
 document.querySelector(".modal-close").addEventListener( "click", ()=>{
     document.querySelector(".modal").style.display = "none";
 });
+
+//type of calc (mul or sum)
 let sumOrMul="sum";
+
+//list  sequence in first row
 let inputsOneRow=document.querySelectorAll(".calc-row_one");
+
+//list  sequence in second row
 let inputsTwoRow=document.querySelectorAll(".calc-row_two");
+
+//list  sequence in third row
 let inputsThreeRow=document.querySelectorAll(".calc-row_three");
+
+/**
+ * function which sum sequence of numbers
+ * @param {Object} list
+ * @return {Number}
+ */
 
 function sum(list){
     let result=0;
@@ -19,6 +35,12 @@ function sum(list){
 
     return result;
 }
+
+/**
+ * function which  multiply sequence of numbers
+ * @param {Object} list
+ * @return {Number}
+ */
 
 function mul(list){
     let result=1;
@@ -32,14 +54,18 @@ function mul(list){
     return result;
 }
 
+//switch calc to sum
 document.querySelector(".calc-sum").addEventListener( "click", ()=>{
     sumOrMul='sum';
 });
 
+
+//switch calc to mul
 document.querySelector(".calc-mul").addEventListener( "click", ()=>{
     sumOrMul='mul';
 });
 
+//count results in first row
 inputsOneRow.forEach(index=>{
     index.addEventListener('input', ()=>
     {
@@ -55,6 +81,7 @@ inputsOneRow.forEach(index=>{
     });
 });
 
+//count results in second row
 inputsTwoRow.forEach(index=>{
     index.addEventListener('input', ()=>
     {
@@ -71,6 +98,7 @@ inputsTwoRow.forEach(index=>{
     });
 });
 
+//count results in third row
 inputsThreeRow.forEach(index=>{
     index.addEventListener('input', ()=>
     {
@@ -86,14 +114,17 @@ inputsThreeRow.forEach(index=>{
     });
 });
 
+//mark a third row 
 document.querySelector('.mark-row_three').addEventListener('click', ()=>{
     document.querySelector(".row-three").classList.toggle("bg-green");
 });
 
+//mark a second row 
 document.querySelector('.mark-row_two').addEventListener('click', ()=>{
     document.querySelector(".row-two").classList.toggle("bg-green");
 });
 
+//mark a first row 
 document.querySelector('.mark-row_one').addEventListener('click', ()=>{
     document.querySelector(".row-one").classList.toggle("bg-green");
 });
